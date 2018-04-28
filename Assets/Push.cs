fc706a744e5ae452843047851c1a025ac8fccbcf
Kirk McCulloch <kirkdmcm@gmail.com>
2018-04-28 14:45:45 -0600

    initial commit

---
+++ Assets/Push.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Push : MonoBehaviour {
+
+    public Vector3 angle;
+    public float thrust;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        Invoke("DoPush", 2);
+    }
+
+    private void DoPush() {
+        rb.AddForce(angle * thrust);
+    }
+
+    private void Update() {
+        if (Input.GetKey("escape")) {
+            Application.Quit();
+        }
+            
+    }
+
+}
